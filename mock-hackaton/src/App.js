d1a5285b889bbb601093833203b441a5a58a5e1b
Katia <katiacuevasj@gmail.com>
2022-01-10 20:01:13 -0600

    hace cambios

--- mock-hackaton/src/App.js
+++ mock-hackaton/src/App.js
@@ -3,7 +3,8 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-     
+     <h1>hola soy katia haciendo cambios</h1>
+     <p> vamos a ver si hace pull</p>
     </div>
   );
 }
