29903269883f58a4714eb8db1916a086622c049b
Katia <katiacuevasj@gmail.com>
2022-01-11 20:58:50 -0600

    hace categorias

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Category from "./components/Category";
 import { Header } from "./components/Header";
+
 
 function App() {
   return (
+    <> 
     <Header />
+     <Category />
+     </>
   );
 }
 
