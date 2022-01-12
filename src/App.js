033ded6d8a8b673bf4df57013bc6afe90557012f
MelLeyva <mleyva0795@gmail.com>
2022-01-11 18:46:48 -0600

    body home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Header } from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Home />
+    </>
   );
 }
 
