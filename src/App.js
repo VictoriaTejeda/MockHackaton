3359ee58181dd03515099960a5f189cd79f08306
MelLeyva <mleyva0795@gmail.com>
2022-01-11 22:41:01 -0600

    rutas admin categorria y todas las categrias

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React from "react";
 import Category from "./components/Category";
 import { Header } from "./components/Header";
 import Home from "./components/Home";
-import Footer from "./components/Footer"
-
-
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AllCategories from "./components/AllCategories";
 
 function App() {
   return (
     <>
-      <header><Header /></header>
-      <Home />
-        <Category />
-      <footer><Footer /></footer>
-      </>
+      <header>
+        <Header />
+      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="category" element={<Category />} />
+          <Route exact path="allCategories" element={<AllCategories />} />
+        </Routes>
+      </Router>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   );
 }
 
