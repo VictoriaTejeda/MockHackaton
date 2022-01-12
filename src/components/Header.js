02922422cd6ce8add1853214365c4d0db39fa06c
MelLeyva <mleyva0795@gmail.com>
2022-01-11 20:51:27 -0600

    merge of modal and home

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,24 @@ import { ModalWindow } from "./ModalWindow";
 export const Header = () => {
   const { handleClose, handleOpen, open } = useTools();
   return (
-    <div>
-      <button onClick={handleOpen}>Pinchame</button>
-      <ModalWindow
-        open={open}
-        handleClose={handleClose}
-        title={`Provide username and password`}
-      >
+    <>
+      <nav className='header'>
+        <img className='logo' src="https://i.ibb.co/P9cWzMK/Rectangle-3.png" alt="Rectangle-3" border="0" />
+        <div className='navButtons'>
+          <button className='loginButton' onClick={handleOpen}> <img src="https://i.ibb.co/KDGYvy2/usuario-1-1.png" alt="usuario-1-1" border="0" />
+            Login</button>
+          {/* <BotonPrueba /> */}
+          <ModalWindow
+          open={open}
+          handleClose={handleClose}
+          title={`Provide username and password`}
+        >
           <FormLogin />
-      </ModalWindow>
-    </div>
+        </ModalWindow>
+          <img className='car' src="https://i.ibb.co/gMbFBwN/carro-de-la-carretilla-1.png" alt="carro-de-la-carretilla-1" border="0"
+            height={47} width={53} />
+        </div>
+      </nav>
+    </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './modal.css';
+import './modal.scss';
 import App from './App';
 
 
